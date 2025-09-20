3c965ff6ff1356ac3043e149042625da0e6e885f
Srujan <srujansmurty@gmail.com>
2025-09-20 20:25:27 +0530

    Login and sign up auth :)

---
+++ project-root/frontend/src/app/components/trail-mode/trail-mode/trail-mode.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trail-mode',
+  imports: [],
+  standalone: true,
+  templateUrl: './trail-mode.html',
+})
+export class TrailMode {
+
+}
---
+++ project-root/frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyATGhBgI62TF0acM926zttKJygwoXdik4c",
+  authDomain: "startologer.firebaseapp.com",
+  projectId: "startologer",
+  storageBucket: "startologer.firebasestorage.app",
+  messagingSenderId: "653175173149",
+  appId: "1:653175173149:web:80231fb6423153aad956f4",
+  measurementId: "G-9HTD1Y1B7S"
+  }
+};
